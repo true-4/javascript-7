f27f36f3da25f7d5e868e512144702dfd29de2e9
kuga <esixterb58@gmail.com>
2025-05-25 12:05:30 +0300

    feat: task 4

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ getThis() // возвращает undefined так как в строгом ре
 
 const arrThis = () => console.log(this)
 arrThis() // так как не имеет собственного this ссылается на LE родителя (документа)
+
+// 4
+function formatName(name) {
+  const firstLit = name.charAt(0).toUpperCase()
+  const restLit = name.slice(1).toLowerCase()
+  return firstLit + restLit
+}
+console.log(formatName('alEX'))
