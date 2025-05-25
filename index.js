ff7ff4f362c85ce47d126edf4a43ecd64ca8af3f
kuga <esixterb58@gmail.com>
2025-05-25 11:02:04 +0300

    feat: task 2 use strict

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// NO 'use strict'
+'use strict'
 
 console.log(this) // ссылается на LE документа
 
 function getThis() {
   console.log(this)
 }
-getThis() // ссылается на LE документа так как не находит собственный this
+getThis() // возвращает undefined так как в строгом режиме function declaration всегда что то возвращает и имеет this
 
 const arrThis = () => console.log(this)
-arrThis() // так как не имеет собственного this ссылается на LE родителя (документа0
+arrThis() // так как не имеет собственного this ссылается на LE родителя (документа)
